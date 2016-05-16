e05c4d108e8bccb09c13c1a2aade1f881592f591
林义凯 <externIE@outlook.com>
2016-05-16 17:36:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/externie/exclockdemo/ClockActivity.java
@@ -0,0 +1,13 @@
+package com.example.externie.exclockdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock);
+    }
+}
